29e369c800a27bb0247a255440412cdfc2e47630
wangyu33 <1134711093@qq.com>
2020-09-11 11:06:23 +0800

    LeetCode

--- LeetCode40.py
+++ LeetCode40.py
@@ -6,35 +6,35 @@
 
 from typing import List
 class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates = sorted(candidates)
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        target = n
+        candidates = [1,2,3,4,5,6,7,8,9]
         ans = []
 
         def dfs(tar, start, temp: List[int]):
-            if start >= len(candidates):
+            if len(temp) > k:
+                return
+            if len(temp) == k and sum(temp) == target:
+                ans.append(temp)
+                return
+            elif len(temp) == k and sum(temp) != target:
                 return
             for i in range(start, len(candidates)):
                 num = candidates[i]
                 if num + tar > target:
                     break
                 if (target - tar) // num > 0:
-                    for j in range(1, min((target - tar) // num + 1, 2)):
-                        if tar + num * j == target:
-                            gao = temp[:]
-                            gao.extend([num] * j)
-                            ans.append(gao)
-                            continue
-                        else:
-                            gao = temp[:]
-                            gao.extend([num] * j)
-                            dfs(tar + num * j, i + 1, gao)
+                    gao = temp[:]
+                    gao.extend([num])
+                    dfs(tar + num, i + 1, gao)
 
         dfs(0, 0, [])
         dic = list(set([tuple(t) for t in ans]))
         ans = list(list(t) for t in dic)
         return ans
 
-candidates = [2,5,2,1,2]
-target = 5
+
+k = 3
+n = 9
 S = Solution()
-print(S.combinationSum2(candidates, target))
+print(S.combinationSum3(k, n))
